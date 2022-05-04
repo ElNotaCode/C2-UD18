be0821c49ddacabe00ec2c13bebd4e3d09561fa1
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-04 18:22:03 +0200

    ConnectionDB definido con sus metodos y Ej1 empezado

--- TA18/src/ej1/Ej1App.java
+++ TA18/src/ej1/Ej1App.java
@@ -3,13 +3,55 @@ package ej1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import utils.ConnectionDB;
 
 public class Ej1App {
 
+	//la hacemos static para poder usarla desde los metodos
+	static Connection conexion = null;
+	
 	public static void main(String[] args) {
 
+		conexion = ConnectionDB.crearConexion("192.168.1.140","remote","Reus_2022");
+		
+		ConnectionDB.dropIfExistsDB("tienda_informatica");
+		ConnectionDB.createDB("tienda_informatica");
+		ConnectionDB.useDB("tienda_informatica");
+		
+		ConnectionDB.executeQuery("CREATE TABLE fabricantes(codigo_fabricante INT AUTO_INCREMENT PRIMARY KEY,\r\n"
+				+ "		nombre NVARCHAR(100)\r\n"
+				+ "		);");
+		
+		ConnectionDB.executeQuery("CREATE TABLE articulos(codigo_articulo INT AUTO_INCREMENT PRIMARY KEY,\r\n"
+				+ "		nombre NVARCHAR(100),\r\n"
+				+ "		precio INT DEFAULT 100,\r\n"
+				+ "		codigo_fabricante INT,\r\n"
+				+ "		KEY (codigo_fabricante),\r\n"
+				+ "		FOREIGN KEY (codigo_fabricante) REFERENCES fabricantes(codigo_fabricante)\r\n"
+				+ "		ON UPDATE CASCADE ON DELETE CASCADE\r\n"
+				+ "		);");
+		
+		
 		
 
+		ConnectionDB.closeConnection();
 	}
+	
 
+
+		/*CREATE TABLE fabricantes(codigo_fabricante INT AUTO_INCREMENT PRIMARY KEY,
+		nombre NVARCHAR(100)
+		);*/
+	
+		/*CREATE TABLE articulos(codigo_articulo INT AUTO_INCREMENT PRIMARY KEY,
+		nombre NVARCHAR(100),
+		precio INT DEFAULT 100,
+		codigo_fabricante INT,
+		KEY (codigo_fabricante),
+		FOREIGN KEY (codigo_fabricante) REFERENCES fabricantes(codigo_fabricante)
+		ON UPDATE CASCADE ON DELETE CASCADE
+		);*/
+	
 }
--- TA18/src/utils/ConnectionDB.java
+++ TA18/src/utils/ConnectionDB.java
@@ -3,56 +3,142 @@ package utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class ConnectionDB {
 
-	static String IP = "192.168.1.140";
-	static String USER = "remote";
-	static String PASSWORD = "Reus_2022";
+	//Autor Eloy Martorell 04/05/2022
+	private static String IP = "192.168.1.140";
+	private static String USER = "remote";
+	private static String PASSWORD = "Reus_2022";
 	private static Connection conexion = null;
+	private static Statement statement = null;
 	
 	//crear conexiones
 	public static Connection crearConexion(String ip,String user,String password) {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conexion = DriverManager.getConnection("jdbc:mysql://"+ ip +":3306?useTimezone=true&serverTimezone=UTC,"+ password +"," + user);
-			System.out.println("Conexion establecida");
+			conexion = DriverManager.getConnection("jdbc:mysql://" + ip + ":3306?useTimezone=true&serverTimezone=UTC", user, password);
+			System.out.println("Conexion establecida.");
+			statement = conexion.createStatement();
 			return conexion;
 		} catch (SQLException |ClassNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println("No se ha podido conectar");
+			System.out.println("No se ha podido conectar.");
 			System.out.println(e);
 			return null;
 		}
 		
-		
 	}
 	
-	//Por defecto con mi ip, user y pass
-	public static Connection crearConexion() {
-		
+	//metodos en orden de procedimiento (crear db, tablas y finalmente cerrar...)
+	//crear base de datos
+	public static void createDB(String name) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conexion = DriverManager.getConnection("jdbc:mysql://"+ IP +":3306?useTimezone=true&serverTimezone=UTC,"+ PASSWORD +"," + USER);
-			System.out.println("Conexion establecida");
-			return conexion;
-		} catch (SQLException |ClassNotFoundException e) {
+			String query = "CREATE DATABASE " + name;
+			statement.executeUpdate(query);
+			System.out.println("DB "+ name +" creada.");
+		} catch (SQLException e) {
+			// ERROR
+			System.out.println(e.getMessage());
+			System.out.println("No se ha podido crear la base de datos.");
+		}
+	}
+	
+	//borrar if exists
+	public static void dropIfExistsDB(String name) {
+		try {
+			String query = "DROP DATABASE IF EXISTS " + name;
+			statement.executeUpdate(query);
+			System.out.println("DB "+ name +" destruida.");
+		} catch (SQLException e) {
+			// ERROR
+			System.out.println(e.getMessage());
+			System.out.println("No se ha podido destruir la base de datos.");
+		}
+	}
+	
+	//borrar base de datos
+	public static void dropDB(String name) {
+		try {
+			String query = "DROP DATABASE " + name;
+			statement.executeUpdate(query);
+			System.out.println("DB "+ name +" destruida.");
+		} catch (SQLException e) {
+			// ERROR
+			System.out.println(e.getMessage());
+			System.out.println("No se ha podido destruir la base de datos.");
+		}
+	}
+	
+	//use db
+	public static void useDB(String name) {
+		try {
+			String query = "USE " + name;
+			statement.executeUpdate(query);
+			System.out.println("Apuntando a la base de datos " + name + ".");
+		} catch (SQLException e) {
+			// ERROR
+			System.out.println(e.getMessage());
+			System.out.println("No se ha podido apuntar a la base de datos.");
+		}
+	}
+	
+	//borrar tabla
+	public static void dropTableIfExists(String name) {
+		try {
+			String query = "DROP TABLE IF EXISTS" + name;
+			statement.executeUpdate(query);
+			System.out.println("Tabla " + name + " destruida.");
+		} catch (SQLException e) {
+			// ERROR
+			System.out.println(e.getMessage());
+			System.out.println("No se ha podido destruir la tabla.");
+		}
+	}
+	
+	//borrar tabla
+	public static void dropTable(String name) {
+		try {
+			String query = "DROP TABLE " + name;
+			statement.executeUpdate(query);
+			System.out.println("Tabla "+ name +" destruida.");
+		} catch (SQLException e) {
+			// ERROR
+			System.out.println(e.getMessage());
+			System.out.println("No se ha podido destruir la tabla.");
+		}
+	}
+	
+	//ejecutar query (para tablas y values)
+	//metodos para crear tablas
+	public static void executeQuery(String query) {
+		/*CREATE TABLE fabricantes(codigo_fabricante INT AUTO_INCREMENT PRIMARY KEY,
+		nombre NVARCHAR(100)
+		);*/
+		try {
+			Statement statement = conexion.createStatement();
+			statement.execute(query);
+			System.out.println("Tabla fabricantes creada.");
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			System.out.println("No se ha podido conectar");
 			System.out.println(e);
-			return null;
+			System.out.println("Error creando la tabla.");
 		}
 		
 		
 	}
 
 	//cerrar conexion
-	void closeConnection() {
+	public static void closeConnection() {
 		try {
 			conexion.close();
+			System.out.println("Conexión cerrada.");
 		} catch (SQLException e) {
 			// TODO: handle exception
+			System.out.println("Error al cerrar la conexión.");
 		}
 	}
 
